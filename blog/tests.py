f579d386644028f52f090eda4af9c6ada42b3989
ryotakoyama <kryota.gin@gmail.com>
2019-12-14 15:20:31 +0900

    トップページアクセス時のステータスコードでアサートをかける単体テスト

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,13 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 
 # Create your tests here.
+
+
+class BlogTestCase(TestCase):
+
+    def setUp(self):
+        self.c = Client()
+
+    def test_index_access(self):
+        response = self.c.get('/')
+        self.assertEqual(200, response.status_code)
